f30d291f01f8c4977ed1e5fd008c830b99698fe7
ndnd07 <dolevsco08@gmail.com>
2025-08-28 15:18:44 +0300

    create the functions

--- src/main/java/frc/robot/Subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/Subsystems/drivetrain/Drivetrain.java
@@ -1,8 +1,12 @@
 package frc.robot.Subsystems.drivetrain;
 
 
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Subsystems.drivetrain.mecanum.Mecanum;
 import frc.robot.Subsystems.drivetrain.swerve.Swerve;
@@ -65,19 +69,67 @@ public abstract class Drivetrain extends SubsystemBase {
     public void drive(ChassisSpeeds speeds) {
         var discreteSpeeds = ChassisSpeeds.discretize(speeds, 0.02);
         setChassisSpeed(discreteSpeeds);
-        Logger.recordOutput("drive train/target speed", discreteSpeeds);
+        Logger.recordOutput("drivetrain/target speed", discreteSpeeds);
     }
 
+    /**
+     * forwards the speeds to the implementation
+     *
+     * @param speeds the target speed of the robot
+     */
     protected abstract void setChassisSpeed(ChassisSpeeds speeds);
 
-
+    /**
+     * return the latest gyro angle
+     * (counter clockwise positive)
+     *
+     * @return the gyro angle
+     */
     public Rotation2d getGyroAngle() {
         return inputs.gyroAngle;
     }
 
+    /**
+     * return the latest speeds of the robot
+     *
+     * @return speeds
+     */
     public ChassisSpeeds getChassisSpeeds() {
         return inputs.speeds;
     }
+
+    /**
+     * adds the vision measurement
+     *
+     * @param pose      the position the vision think the robot is there
+     * @param timestamp the time when the pose was taken
+     * @param stdDevs   A Vector with 3 parameters in the following order:
+     *                  X standard deviation (in meters).
+     *                  Y standard deviation (in meters).
+     *                  Theta standard deviation (in radians).
+     */
+    public abstract void addVisionMeasurement(Pose2d pose, double timestamp, Matrix<N3, N1> stdDevs);
+
+    /**
+     * return the latest position of the robot
+     *
+     * @return the latest pose
+     */
+    public abstract Pose2d getEstimatedPosition();
+
+    @Override
+    public void periodic() {
+        updateInputs(inputs);
+        Logger.processInputs("drivetrain", inputs);
+    }
+
+    /**
+     * update the inputs from the sensors
+     * @param inputs the inputs that stores the data
+     */
+
+    protected abstract void updateInputs(DrivetrainInputs inputs);
+
 
 }
 
