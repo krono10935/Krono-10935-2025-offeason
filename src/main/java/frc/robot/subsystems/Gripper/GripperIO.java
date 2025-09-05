a44ddba0ea1ddee8dbe949e100dbc2a9e5e0978f
Antimatter dimensions copycat <roniyaakobi@yahoo.com>
2025-09-05 13:43:59 +0300

    Turned the abstract IO class back to an interface and built the sim implementation

--- src/main/java/frc/robot/subsystems/Gripper/GripperIO.java
+++ src/main/java/frc/robot/subsystems/Gripper/GripperIO.java
@@ -2,51 +2,37 @@ package frc.robot.subsystems.Gripper;
 
 import org.littletonrobotics.junction.AutoLog;
 
-import io.github.captainsoccer.basicmotor.BasicMotor;
-import io.github.captainsoccer.basicmotor.controllers.Controller.ControlMode;
-
-public abstract class GripperIO {
+public interface GripperIO {
     @AutoLog
-    public static class GripperInputs {
+    class GripperInputs {
         double temperature;
     }
-
-    public BasicMotor motor;
 
     /**
      * Set torque for the grip on algae
      * @param torque
      */
-    public void setTorque(double torque){
-        motor.setControl(torque, ControlMode.TORQUE, 1);
-    } 
+    public void setTorque(double torque); 
 
     /**
      * Keep your position to hold the coral
      */
-    public void keepPosition() {
-        // Use slot 0 of the PIDF config for the position mode
-        motor.setControl(motor.getPosition(), ControlMode.POSITION);
-    }
+    public void keepPosition();
 
     /**
      * Set percent output for intake or ejection of coral
      * @param percent
      */
-    public void setPercentOutput(double percent) {
-        motor.setPrecentOutput(percent);
-    }
+    public void setPercentOutput(double percent);
 
     /**
      * Used to let go of the algae
      */
-    public void stopMotor() {
-        motor.stop();
-    }
+    public void stopMotor();
 
     /**
      * Update the GripperInputsAutologged
      * @param inputs
      */
-    public abstract void updateInputs(GripperInputs inputs);
+    public void updateInputs(GripperInputs inputs);
 }
