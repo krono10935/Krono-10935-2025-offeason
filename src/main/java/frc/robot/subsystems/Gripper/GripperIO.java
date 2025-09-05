540d1c6eb3e5df455a7ce11a4cb36abe741316e8
Antimatter dimensions copycat <roniyaakobi@yahoo.com>
2025-09-05 12:26:52 +0300

    Created sim gripper and changed io from interface to abstract class

--- src/main/java/frc/robot/subsystems/Gripper/GripperIO.java
+++ src/main/java/frc/robot/subsystems/Gripper/GripperIO.java
@@ -2,37 +2,51 @@ package frc.robot.subsystems.Gripper;
 
 import org.littletonrobotics.junction.AutoLog;
 
-public interface GripperIO {
+import io.github.captainsoccer.basicmotor.BasicMotor;
+import io.github.captainsoccer.basicmotor.controllers.Controller.ControlMode;
+
+public abstract class GripperIO {
     @AutoLog
-    class GripperInputs {
+    public static class GripperInputs {
         double temperature;
     }
+
+    public BasicMotor motor;
 
     /**
      * Set torque for the grip on algae
      * @param torque
      */
-    public void setTorque(double torque); 
+    public void setTorque(double torque){
+        motor.setControl(torque, ControlMode.TORQUE, 1);
+    } 
 
     /**
      * Keep your position to hold the coral
      */
-    public void keepPosition();
+    public void keepPosition() {
+        // Use slot 0 of the PIDF config for the position mode
+        motor.setControl(motor.getPosition(), ControlMode.POSITION);
+    }
 
     /**
      * Set percent output for intake or ejection of coral
      * @param percent
      */
-    public void setPercentOutput(double percent);
+    public void setPercentOutput(double percent) {
+        motor.setPrecentOutput(percent);
+    }
 
     /**
      * Used to let go of the algae
      */
-    public void stopMotor();
+    public void stopMotor() {
+        motor.stop();
+    }
 
     /**
      * Update the GripperInputsAutologged
      * @param inputs
      */
-    public void updateInputs(GripperInputs inputs);
+    public abstract void updateInputs(GripperInputs inputs);
 }
--- src/main/java/frc/robot/subsystems/Gripper/GripperIOSim.java
+++ src/main/java/frc/robot/subsystems/Gripper/GripperIOSim.java
@@ -1,28 +1,10 @@
 package frc.robot.subsystems.Gripper;
 
-public class GripperIOSim implements GripperIO {
-    @Override
-    public void setTorque(double torque) {
-        // TODO Auto-generated method stub
-        
-    }
+import io.github.captainsoccer.basicmotor.sim.motor.BasicMotorSim;
 
-    @Override
-    public void setPercentOutput(double percent) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void stopMotor() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void keepPosition() {
-        // TODO Auto-generated method stub
-        
+public class GripperIOSim extends GripperIO {
+    public GripperIOSim(){
+        motor = new BasicMotorSim(GripperConstants.motorConfig);
     }
 
     @Override
