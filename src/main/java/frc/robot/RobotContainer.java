42b23ab15c257586a6203f85c12564ed4ab79ae6
Roni <roniyaakobi@yahoo.com>
2025-09-23 18:28:40 +0300

    reverted mistake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.Subsystems.Arm.ArmSubsystem;
-import frc.robot.commands.setArmPositionCommand;
 import frc.robot.commands.Gripper.HoldCommand;
 import frc.robot.subsystems.Gripper.Gripper;
 
